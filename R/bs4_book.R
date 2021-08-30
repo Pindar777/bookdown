5b7be1547010fc0c5cdad7016d2c99170b68d0d8
Leon Unger <44449736+Pindar777@users.noreply.github.com>
2021-08-30 17:46:10 +0200

    enable switch fn_gitbook 2

--- R/bs4_book.R
+++ R/bs4_book.R
@@ -261,7 +261,7 @@ bs4_chapter_tweak <- function(path, toc, rmd_index = NULL, repo = NULL, fn_gitbo
   tweak_chapter(html)
   tweak_anchors(html)
   tweak_chunks(html)
-  tweak_footnotes(html, fn_gitbook = fn_gitbook)
+  tweak_footnotes(html, fn_gitbook)
   tweak_part_screwup(html)
   tweak_navbar(html, toc, basename(path), rmd_index = rmd_index, repo = repo)
   tweak_metadata(html, path)
